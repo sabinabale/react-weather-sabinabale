438945019e55e662186d16ffcf52d6fabe3addf9
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-09-25 20:12:22 +0200

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+      </div>
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Prague</h1>
+      <ul>
+        <li>Monday 07:00</li>
+        <li>Sunny</li>
+      </ul>
+    </div>
+  );
+}
